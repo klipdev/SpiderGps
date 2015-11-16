d22e1205d91099b5619572cc0faca7b3126182d3
klipdev <klipdev@gmail.com>
2015-11-16 21:35:06 +0100

    Updated license path for JxBrowser

--- workspace/SpiderGps/src/org/klipdev/spidergps/SGAboutPanel.java
+++ workspace/SpiderGps/src/org/klipdev/spidergps/SGAboutPanel.java
@@ -10,7 +10,7 @@ public class SGAboutPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = -3731770414268130528L;
-	final String jxBrowserDisclaimer = "";
+	final String jxBrowserDisclaimer = "SpiderGps uses JxBrowser http://www.teamdev.com/jxbrowser, which is a proprietary software. The use of JxBrowser is governed by JxBrowser Product Licence Agreement http://www.teamdev.com/jxbrowser-licence-agreement. If you would like to use JxBrowser in your development, please contact TeamDev.";
 	
 	public SGAboutPanel() {
 		buildPanel();
